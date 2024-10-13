9c0360202ad7eb9e81093772ec5b2dcb277ebd76
perpel <572821520@qq.com>
2024-10-13 11:35:36 +0800

    m

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kordar/async-task-starter
 go 1.16
 
 require (
+	github.com/kordar/gologger v0.0.8
 	github.com/kordar/gotask v0.0.6
 	github.com/spf13/cast v1.6.0
 )
--- starter.go
+++ starter.go
@@ -1,19 +1,32 @@
 package async_task_starter
 
 import (
+	logger "github.com/kordar/gologger"
 	"github.com/kordar/gotask"
 	"github.com/spf13/cast"
 )
 
 type AsyncTaskStarter struct {
+	name string
+	load func(moduleName string, itemId string, item map[string]string)
+}
+
+func NewAsyncTaskStarter(name string, load func(moduleName string, itemId string, item map[string]string)) *AsyncTaskStarter {
+	return &AsyncTaskStarter{name, load}
 }
 
 func (m AsyncTaskStarter) Name() string {
-	return "async_task_starter"
+	return m.name
 }
 
 func (m AsyncTaskStarter) Load(value interface{}) {
-	cfg := cast.ToStringMap(value)
+	cfg := cast.ToStringMapString(value)
+	id := cfg["id"]
+	if id == "" {
+		logger.Fatalf("[%s] the attribute id cannot be empty.", m.Name())
+		return
+	}
+
 	workpoolsize := 3
 	if cfg["work_size"] != "" {
 		workpoolsize = cast.ToInt(cfg["work_size"])
@@ -25,6 +38,13 @@ func (m AsyncTaskStarter) Load(value interface{}) {
 	}
 
 	gotask.InitTaskHandle(workpoolsize, workpoolbuflen)
+
+	if m.load != nil {
+		m.load(m.name, id, cfg)
+		logger.Debugf("[%s] triggering custom loader completion", m.Name())
+	}
+
+	logger.Infof("[%s] loading module '%s' successfully", m.Name(), id)
 }
 
 func (m AsyncTaskStarter) Close() {
